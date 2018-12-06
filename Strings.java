0d5cf5a3ac62d25ae33b241cff756e907a6a1c29
Kris <36204796+kdale2@users.noreply.github.com>
2018-12-06 16:07:27 -0600

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -21,20 +21,26 @@ public String makeTags(String tag, String word) {
   return one;
 }
 
-//Given an "out" string length 4, such as "<<>>", and a word, return a new string where //the word is in the middle of the out string, e.g. "<<word>>". Note: use //str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+//Given an "out" string length 4, such as "<<>>", and a word, return a new string where 
+//the word is in the middle of the out string, e.g. "<<word>>". Note: use 
+//str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
 
 public String makeOutWord(String out, String word) {
   return out.substring(0,2) + word + out.substring(2,4);
 }
 
-//Given a string, return a new string made of 3 copies of the last 2 chars of the //original string. The string length will be at least 2.
+//Given a string, return a new string made of 3 copies of the last 2 chars of the 
+//original string. The string length will be at least 2.
 
 public String extraEnd(String str) {
   String end = str.substring(str.length()-2, str.length());
   return end + end + end;
 }
 
-//Given a string, return the string made of its first two chars, so the String "Hello" //yields "He". If the string is shorter than length 2, return whatever there is, so "X" //yields "X", and the empty string "" yields the empty string "". Note that str.length() //returns the length of a string.
+//Given a string, return the string made of its first two chars, so the String "Hello" 
+//yields "He". If the string is shorter than length 2, return whatever there is, so "X" 
+//yields "X", and the empty string "" yields the empty string "". Note that str.length() 
+//returns the length of a string.
 
 public String firstTwo(String str) {
   if (str.length() < 2) {
@@ -58,7 +64,9 @@ public String withoutEnd(String str) {
   return str.substring(1, str.length()-1);
 }
 
-//Given 2 strings, a and b, return a string of the form short+long+short, with the //shorter string on the outside and the longer string on the inside. The strings will //not be the same length, but they may be empty (length 0).
+//Given 2 strings, a and b, return a string of the form short+long+short, with the 
+//shorter string on the outside and the longer string on the inside. The strings will 
+//not be the same length, but they may be empty (length 0).
 
 public String comboString(String a, String b) {
   if (a.length()<1) {
@@ -75,7 +83,8 @@ public String comboString(String a, String b) {
   }
 }
 
-//Given 2 strings, return their concatenation, except omit the first char of each. The //strings will be at least length 1.
+//Given 2 strings, return their concatenation, except omit the first char of each. The 
+//strings will be at least length 1.
 
 public String nonStart(String a, String b) {
   String one = a.substring(1);
@@ -83,7 +92,8 @@ public String nonStart(String a, String b) {
   return one + two;
 }
 
-//Given a string, return a "rotated left 2" version where the first 2 chars are moved to //the end. The string length will be at least 2.
+//Given a string, return a "rotated left 2" version where the first 2 chars are moved to 
+//the end. The string length will be at least 2.
 
 public String left2(String str) {
   String new1 = str.substring(2);
@@ -91,7 +101,8 @@ public String left2(String str) {
   return new1 + front;
 }
 
-//Given a string, return a string length 1 from its front, unless front is false, in //which case return a string length 1 from its back. The string will be non-empty.
+//Given a string, return a string length 1 from its front, unless front is false, in 
+//which case return a string length 1 from its back. The string will be non-empty.
 
 public String theEnd(String str, boolean front) {
   if (front == false) {
@@ -101,9 +112,3 @@ public String theEnd(String str, boolean front) {
     return str.substring(0,1);
   }
 }
-
-
-
-
-
-
