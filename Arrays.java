db4770dbb53ee5eca5c62bd95b141902dfef449d
Kris <36204796+kdale2@users.noreply.github.com>
2019-10-25 11:28:19 -0500

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,11 +1,11 @@
-/* 1 -Given an array of ints, return true if 6 appears as either the first or 
+/* 1 - Given an array of ints, return true if 6 appears as either the first or 
 last element in the array. The array will be length 1 or more. */
 
 public boolean firstLast6(int[] nums) {
   return nums[0]==6 || nums[nums.length-1]==6;
 }
 
-/* 2- Given an array of ints, return true if the array is length 1 or more, 
+/* 2 - Given an array of ints, return true if the array is length 1 or more, 
 and the first element and the last element are equal. */
 
 public boolean sameFirstLast(int[] nums) {
@@ -24,7 +24,7 @@ public boolean sameFirstLast(int[] nums) {
   return same;
 }
 
-// 3- Return an int array length 3 containing the first 3 digits of pi {3, 1, 4}.
+// 3 - Return an int array length 3 containing the first 3 digits of pi {3, 1, 4}.
 
 public int[] makePi() {
   int[] pi = new int[3];
@@ -34,7 +34,7 @@ public int[] makePi() {
   return pi;
 }
 
-/* 4- Given 2 arrays of ints, a and b, return true if they have the 
+/* 4 - Given 2 arrays of ints, a and b, return true if they have the 
 same first element or they have the same last element. Both arrays 
 will be length 1 or more. */
 
@@ -42,7 +42,7 @@ public boolean commonEnd(int[] a, int[] b) {
   return a[0] == b[0] || a[a.length-1] == b[b.length-1];
 }
 
-// 5- Given an array of ints length 3, return the sum of all the elements.
+// 5 - Given an array of ints length 3, return the sum of all the elements.
 
 public int sum3(int[] nums) {
   int sum=0;
@@ -53,7 +53,7 @@ public int sum3(int[] nums) {
   return sum;
 }
 
-/* 6- Given an array of ints length 3, return an array with the 
+/* 6 - Given an array of ints length 3, return an array with the 
 elements "rotated left" so {1, 2, 3} yields {2, 3, 1}. */
 
 public int[] rotateLeft3(int[] nums) {
@@ -68,7 +68,7 @@ public int[] rotateLeft3(int[] nums) {
   return newArr;
 }
 
-/* 7- Given an array of ints length 3, return a new array with the 
+/* 7 - Given an array of ints length 3, return a new array with the 
 elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}. */
 
 public int[] reverse3(int[] nums) {
@@ -84,7 +84,7 @@ public int[] reverse3(int[] nums) {
   return arr;
 }
 
-/* 8- Given an array of ints length 3, figure out which is larger, 
+/* 8 - Given an array of ints length 3, figure out which is larger, 
 the first or last element in the array, and set all the other 
 elements to be that value. Return the changed array. */
 
@@ -106,7 +106,7 @@ public int[] maxEnd3(int[] nums) {
   return arr;
 }
 
-/* 9- Given an array of ints, return the sum of the first 2 elements 
+/* 9 - Given an array of ints, return the sum of the first 2 elements 
 in the array. If the array length is less than 2, just sum up the 
 elements that exist, returning 0 if the array is length 0. */
 
