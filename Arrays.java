5ebf86c764ba7288d384fdc879380697379e6cf6
Kris <36204796+kdale2@users.noreply.github.com>
2019-10-25 18:47:41 -0500

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -330,3 +330,26 @@ public boolean unlucky1(int[] nums) {
   }
   return unlucky;
 }
+
+/* 24 - Given 2 int arrays, a and b, return a new array length 2 containing, as much as will fit, 
+the elements from a followed by the elements from b. The arrays may be any length, including 0, but 
+there will be 2 or more elements available between the 2 arrays. */
+
+public int[] make2(int[] a, int[] b) {
+  
+  int[] newArray = new int[2];
+  
+  if(a.length == 0) {
+    newArray[0] = b[0];
+    newArray[1] = b[1];
+  }
+  if (a.length > 1) {
+    newArray[0] = a[0];
+    newArray[1] = a[1];
+  }
+  if(a.length == 1) {
+    newArray[0] = a[0];
+    newArray[1] = b[0];
+  }
+  return newArray;
+}
