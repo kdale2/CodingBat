378fe40a1c173cb0d77ef3afd35e31d8e270e851
Kris <36204796+kdale2@users.noreply.github.com>
2019-10-28 18:42:01 -0500

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -126,3 +126,43 @@ public String withouEnd2(String str) {
     return str.substring(1, str.length()-1);
   }
 }
+
+/* Given a string, return true if it ends in "ly". */
+
+public boolean endsLy(String str) {
+  
+  int n = str.length();
+
+  if (n < 2) {
+    return false;
+  }
+  else if (str.substring(n-2, n).equals("ly")) {
+    return true;
+  }
+  return false;
+}
+
+/* Given a string and an int n, return a string made of the first and 
+last n chars from the string. The string length will be at least n. */
+
+public String nTwice(String str, int n) {
+  
+  String firstHalf = str.substring(0, n);
+  String lastHalf = str.substring(str.length()-n, str.length());
+  String full = firstHalf + lastHalf;
+  return full;
+}
+
+/* Given a string and an index, return a string length 2 starting at the given index. 
+If the index is too big or too small to define a string length 2, use the first 2 
+chars. The string length will be at least 2. */
+
+public String twoChar(String str, int index) {
+
+  if (index < 0 || index > str.length()-2) {  
+    return str.substring(0,2);
+  }
+  else {
+    return str.substring(index, index + 2);
+  }
+}
